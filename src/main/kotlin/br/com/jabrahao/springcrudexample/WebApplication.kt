70e94ec505bf60bb4ea27b81d386f71b05de5c51
João Abrahão <jribeiroabrahao@gmail.com>
2020-11-19 22:15:23 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/jabrahao/springcrudexample/WebApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jabrahao.springcrudexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCrudExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCrudExampleApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jabrahao/springcrudexample/SpringCrudExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jabrahao.springcrudexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCrudExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
